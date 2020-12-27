250cf364eaf8e5a095b3b07978f28bc419293381
Liam Tengelis <liam@tengelisconsulting.com>
2020-12-27 11:34:33 -0500

    begin defining in scala

--- codegen/define.py
+++ codegen/define.py
@@ -19,3 +19,32 @@ def get_calling_sql(schema: str, fn: FnRecord) -> str:
       {arg_sql}
     )
     """
+
+def type_lookup(typename: str) -> str:
+    return {
+        "text": "String",
+        "integer": "Int",
+    }[typename]
+
+
+def get_impl_language_args(fn: FnRecord) -> str:
+    def fmt_arg(arg: FnArg) -> str:
+        arg_type = type_lookup(arg.type)
+        return f"{arg.name}: {arg_type}"
+    arg_s = ", ".join([fmt_arg(arg) for arg in fn.args])
+    return arg_s
+
+
+def get_impl_language_def(schema: str, fn: FnRecord) -> str:
+    calling_sql = get_calling_sql(schema, fn)
+    impl_fn_args = get_impl_language_args(fn)
+    impl_query_args = ", ".join([a.name for a in fn.args])
+    impl = f"""
+def {fn.name}(con: Connection, {impl_fn_args}) = {{
+  val statement = con.createStatement()
+  val resultSet = statement.executeQuery({calling_sql}
+    , [{impl_query_args}]
+  )
+  resultSet
+}}"""
+    return impl
